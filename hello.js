ee2d71d681bf5a7974a68269f324e8873276072e
kmrajibfaysal <kmrajibfaysal@gmail.com>
2024-04-29 00:43:00 +0600

    added more practice files

---
+++ hello.js
@@ -0,0 +1,20 @@
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const combined = [...arr1, ...arr2];
+
+// console.log(combined);
+
+const firstName = 'Mondol';
+const lastName = 'Pramanik';
+
+const fullName = firstName + ' ' + lastName;
+
+// console.log(fullName);
+
+const price1 = '31';
+
+const priceNumber = parseInt(price1);
+
+// console.log(price1, priceNumber);
+console.log(typeof priceNumber);
